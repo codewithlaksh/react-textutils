9fd05d84efd02f80e46105454f7580124ab8bddc
codewithlaksh <dashlakshyaraj2006@gmail.com>
2022-07-22 18:09:26 +0530

    Fixed minor bug in reading time

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -51,7 +51,7 @@ export default function TextForm(props) {
         <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h1>Your text summary</h1>
             <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-            <p>{0.008 * text.split(" ").filter((element) => {return element.length!==0}).length} Minutes read</p>
+            <p>{0.008 * text.split(/\s+/).filter((element) => {return element.length!==0}).length} Minutes read</p>
             <h2>Preview</h2>
             <p>{text.length>0?text:"Enter something in the textbox above to preview it here"}</p>
         </div>
